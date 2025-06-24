9ab8b2e174f50cdd235230abaa15e9c4670a840e
BlaineCowen <80175841+BlaineCowen@users.noreply.github.com>
2025-06-24 22:21:08 +0200

    fix bun build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
 
 // No adapter needed for basic static site generation
 export default defineConfig({
   output: "server",
+  adapter: vercel({
+    analytics: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [svelte(), tailwind()],
   vite: {
     resolve: {
