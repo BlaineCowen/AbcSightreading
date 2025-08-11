a9d2c51da5fedba597726d9ba105368ce12a5013
BlaineCowen <80175841+BlaineCowen@users.noreply.github.com>
2025-08-11 15:33:20 -0500

    rebuild with sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 
-// Server output needed for API routes
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [svelte(), tailwind()],
   vite: {
-    resolve: {
-      alias: {
-        "@": "./src",
-      },
-    },
     ssr: {
       noExternal: ["abcjs"],
-    },
-    build: {
-      rollupOptions: {
-        external: ["**/*.test.ts"],
-      },
     },
   },
 });
